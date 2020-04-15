635a3d6bce07d0c652733aea50fb5a616daedba8
Sam Olivier <smsolivier@gmail.com>
2020-04-14 22:26:23 -0700

    exe for plotting basis functions

---
+++ exe/basis.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from fem import basis, horner
+
+b = basis.LagrangeBasis(2)
+x = np.linspace(-1,1)
+shape = np.zeros((len(x), b.N))
+
+for i in range(len(x)):
+	shape[i] = horner.PolyVal(b.B, x[i]) 
+
+for i in range(b.N):
+	plt.plot(x, shape[:,i])
+plt.show()
