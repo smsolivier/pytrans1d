c77152428a04776eb765deefe9107bf14b39d48c
Sam Olivier <smsolivier@gmail.com>
2020-05-28 11:44:47 -0700

    spectral equivalence of LOR

--- exe/lor.py
+++ exe/lor.py
@@ -4,8 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from trans1d import * 
+from OutputCycler import OutputCycler 
 
-def Cond(Ne, p):
+oc = OutputCycler()
+
+def KappaMix(Ne, p):
 	xe = np.linspace(0, 1, Ne+1)
 	lob = LobattoBasis(p)
 	leg = LegendreBasis(p-1) if p>1 else LegendreBasis(0) 
@@ -34,16 +37,46 @@ def Cond(Ne, p):
 	prec = spla.inv(Mlow)*M
 	return np.linalg.cond(prec.todense()), np.linalg.cond(M.todense())
 
-Ne = np.array([9, 36, 144])
-p = 3 
-prec = np.zeros(len(Ne))
-orig = np.zeros(len(Ne))
-for i in range(len(Ne)):
-	prec[i], orig[i] = Cond(Ne[i], p) 
+def KappaSecond(Ne, p):
+	xe = np.linspace(0, 1, Ne+1)
+	lob = LobattoBasis(p)
+	h1 = H1Space(xe, lob)
+	h1l = H1Space(h1.x, LobattoBasis(1))
 
-plt.semilogy(Ne, orig, '-o', label='Mixed')
-plt.semilogy(Ne, prec, '-o', label='LOR Prec. Mixed')
-plt.xlabel('Number of Elements')
-plt.ylabel('Condition Number')
-plt.legend()
-plt.show()
+	K = Assemble(h1, WeakPoissonIntegrator, lambda x: 1, 2*p+1).tolil()
+	Kl = Assemble(h1l, WeakPoissonIntegrator, lambda x: 1, 2*p+1).tolil()
+
+	bn = [0,-1]
+	K[bn,:] = 0 
+	K[bn,bn] = 1 
+	Kl[bn,:] = 0 
+	Kl[bn,bn] = 1 
+	K = K.tocsc()
+	Kl = Kl.tocsc()
+
+	prec = spla.inv(Kl)*K
+	return np.linalg.cond(prec.todense()), np.linalg.cond(K.todense())
+
+Ne = np.array([2,4,8,12,24])**2
+p = np.array([2,3,4])
+for i in range(len(p)):
+	mprec = np.zeros(len(Ne))
+	morig = np.zeros(len(Ne))
+	sprec = np.zeros(len(Ne))
+	sorig = np.zeros(len(Ne))
+	for j in range(len(Ne)):
+		mprec[j], morig[j] = KappaMix(Ne[j], p[i]) 
+		sprec[j], sorig[j] = KappaSecond(Ne[j], p[i]) 
+
+	plt.figure()
+	plt.semilogy(Ne, mprec, '-o', label='Mixed LOR Prec.')
+	plt.semilogy(Ne, morig, '-o', label='Mixed HO')
+	plt.semilogy(Ne, sprec, '-o', label='H1 LOR Prec.')
+	plt.semilogy(Ne, sorig, '-o', label='H1 HO')	
+	plt.xlabel('Number of Elements')
+	plt.ylabel('Condition Number')
+	plt.legend(prop={'size':14})
+	if (oc.Good()):
+		plt.savefig(oc.Get())
+if not(oc.Good()):
+	plt.show()
